629fd1674dd59d688a9865790716cb0d0e0d6eeb
Shane Newbill <snewbill@gmail.com>
2017-02-10 15:20:38 -0700

    Fixed the gitIgnore

--- headerPractice/functionPrac.c
+++ headerPractice/functionPrac.c
@@ -1,12 +1,18 @@
-#import <stdio.h>
+#include <stdio.h>
 
-
+int add_together ( int a, int b){
+	return (a + b);
+}
 
 
 
 main(){
+	int firstNumber = 6;
+	int secondNumber = 9;
+
+	int sum = add_together(firstNumber, secondNumber);
 	
+	printf("%i\n", sum);
 
-
-
+	return(0);
 }
